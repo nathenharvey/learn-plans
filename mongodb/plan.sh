ff1835bef4d4e8e325f865e978d1012a80109d86
Nathen Harvey <nharvey@chef.io>
2017-04-06 22:43:19 -0400

    Plan for MongoDB
    Signed-off-by: Nathen Harvey <nharvey@chef.io>

---
+++ mongodb/plan.sh
@@ -0,0 +1,63 @@
+pkg_name=mongodb
+pkg_origin=core
+pkg_version=3.2.10
+pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_description="High-performance, schema-free, document-oriented database"
+pkg_license=('AGPL-3.0')
+pkg_source=http://downloads.mongodb.org/src/${pkg_name}-src-r${pkg_version}.tar.gz
+pkg_shasum=3bef44f50f302159c26194bcac9d51c81d98d57ea728f55400774850a70f5120
+pkg_upstream_url=https://www.mongodb.com/
+pkg_filename=${pkg_name}-src-r${pkg_version}.tar.gz
+pkg_dirname=${pkg_name}-src-r${pkg_version}
+pkg_deps=(core/gcc-libs core/glibc core/openssl)
+pkg_build_deps=(
+  core/coreutils
+  core/gcc
+  core/glibc
+  core/python2
+  core/scons
+  core/openssl
+)
+pkg_bin_dirs=(bin)
+pkg_lib_dirs=(lib)
+pkg_include_dirs=(include)
+pkg_svc_run="mongod --config $pkg_svc_config_path/mongod.conf"
+pkg_svc_user=hab
+pkg_svc_group=hab
+pkg_exports=(
+  [port]=mongod.net.port
+  [address]=sys.ip
+)
+pkg_exposes=(port)
+
+do_prepare() {
+
+    CC="$(pkg_path_for core/gcc)/bin/gcc"
+    CXX="$(pkg_path_for core/gcc)/bin/g++"
+    export CC
+    export CXX
+
+    # create variables for our include and library pathes in a scons friendly format
+    # shellcheck disable=SC2001
+    INCPATH="$(echo "${CFLAGS}" | sed -e "s@-I@@g")"
+    # shellcheck disable=SC2001
+    INCPATH="$(echo "${INCPATH}" | sed -e "s@ @', '@g")"
+    # shellcheck disable=SC2001
+    LIBPATH="$(echo "${LDFLAGS}" | sed -e "s@-L@@g")"
+    # shellcheck disable=SC2001
+    LIBPATH="$(echo "${LIBPATH}" | sed -e "s@ @', '@g")"
+    export LIBPATH
+    export INCPATH
+
+    # because scons dislikes saving our variables, we will save our
+    # variables within the construct ourselves
+    sed -i "840s@**envDict@ENV = os.environ, CPPPATH = ['$INCPATH'], LIBPATH = ['$LIBPATH'], CFLAGS = os.environ['CFLAGS'], CXXFLAGS = os.environ['CXXFLAGS'], LINKFLAGS = os.environ['LDFLAGS'], CC = os.environ['CC'], CXX = os.environ['CXX'], PATH = os.environ['PATH'], **envDict@g" SConstruct
+}
+
+do_build() {
+    scons core --prefix="$pkg_prefix" --ssl -j"$(nproc)"
+}
+
+do_install() {
+    scons install --prefix="$pkg_prefix" --ssl
+}
